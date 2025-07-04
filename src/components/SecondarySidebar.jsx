6a9cf8de58a6d59e77b82dd018426548a8fbb80a
Usman <usmankhan7419@gmail.com>
2025-07-04 19:03:59 +0500

    hosting configuration

--- src/components/SecondarySidebar.jsx
+++ src/components/SecondarySidebar.jsx
@@ -6,28 +6,28 @@ import Sequence from "../assets/sequence.png";
 
 
 const navItems = [
-  { icon: Auto, label: "My Automations", active: true },
-  { icon: Basic, label: "Basic" },
-  { icon: Sequence, label: "Sequences" },
+    { icon: Auto, label: "My Automations", active: true },
+    { icon: Basic, label: "Basic" },
+    { icon: Sequence, label: "Sequences" },
 ];
 
 const SecondarySidebar = () => (
-  <aside className="w-[20%] h-[100%] hidden lg:block">
-    <nav className="flex-1 mt-3">
-      {navItems.map((item) => (
-        <div
-          key={item.label}
-          className={`flex items-center px-8 py-2.5 rounded-lg mx-2 my-1 cursor-pointer
+    <aside className="w-[20%] h-[100%] hidden lg:block">
+        <nav className="flex-1 mt-3">
+            {navItems.map((item) => (
+                <div
+                    key={item.label}
+                    className={`flex items-center px-2 xl:px-8 py-2.5 rounded-lg mx-2 my-1 cursor-pointer
             ${item.active ? "bg-gray-200  text-[#565b65] font-normal font-semibold" : "text-[#565b65] font-semibold hover:bg-gray-200"}
           `}
-        >
-          <img src={item.icon} alt={item.label} />
-          <span className="text-lg ml-3">{item.label}</span>
-        </div>
-      ))}
-    </nav>
-  
-  </aside>
+                >
+                    <img src={item.icon} alt={item.label} />
+                    <span className="text-md xl:text-lg ml-3">{item.label}</span>
+                </div>
+            ))}
+        </nav>
+
+    </aside>
 );
 
 export default SecondarySidebar;
